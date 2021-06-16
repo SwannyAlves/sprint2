9088677ac0f2177d04550eae0ce6144f4fb520d8
Swanny <swannykathllen@gmail.com>
2021-06-16 20:15:00 -0300

    Using Fetch API

--- client/js/app/Scripts.js
+++ client/js/app/Scripts.js
@@ -5,14 +5,14 @@ function handleClick() {
     document.getElementById("menu-icon").src = "assets/close.svg";
     document.getElementById("myside-menu").style.width = "40%";
     document.getElementById("search-input").style.display = "none";
-    document.getElementById("mobile-head").style.height = "5vh";
+    // document.getElementById("mobile-head").style.height = "5vh";
   } else {
     document.getElementById("menu-icon").src = "assets/menu.svg";
     document.getElementById("myside-menu").style.width = "0";
     document.getElementById("header__search").style.display = "flex";
     setTimeout(() => {
       //espera 500 milisegundos antes de voltar com o input
-      document.getElementById("mobile-head").style.height = "auto";
+      // document.getElementById("mobile-head").style.height = "auto";
       document.getElementById("search-input").style.display = "flex";
     }, 250); //500 milisegundos
   }
--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,41 +1,31 @@
 class HttpService {
   get(url) {
     return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-
-      xhr.open('GET', url);
-
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject(xhr.responseText);
-          }
-        }
-      };
-
-      xhr.send();
+      fetch(url)
+        .then((response) => {
+          resolve(response.json());
+        })
+        .catch((error) => {
+          reject(error);
+        });
     });
   }
 
   post(url, data) {
     return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject(xhr.responseText);
-          }
-        }
-      };
-
-      // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-      xhr.send(JSON.stringify(data));
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      })
+        .then((response) => {
+          resolve(response.json());
+        })
+        .catch((error) => {
+          reject(error);
+        });
     });
   }
 }
