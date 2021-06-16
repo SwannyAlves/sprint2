16b9db0cf16d0baf3d24e4b30767feaeb48f72f8
Swanny <swannykathllen@gmail.com>
2021-06-16 18:54:18 -0300

    Header Responsive

---
+++ client/js/app/helpers/Scripts.js
@@ -0,0 +1,19 @@
+function handleClick() {
+  var imageName = document.getElementById("menu-icon").src.split("assets")[1];
+  if (imageName === "/menu.svg") {
+    document.getElementById("header__search").style.display = "none";
+    document.getElementById("menu-icon").src = "assets/close.svg";
+    document.getElementById("myside-menu").style.width = "40%";
+    document.getElementById("search-input").style.display = "none";
+    document.getElementById("mobile-head").style.height = "5vh";
+  } else {
+    document.getElementById("menu-icon").src = "assets/menu.svg";
+    document.getElementById("myside-menu").style.width = "0";
+    document.getElementById("header__search").style.display = "flex";
+    setTimeout(() => {
+      //espera 500 milisegundos antes de voltar com o input
+      document.getElementById("mobile-head").style.height = "auto";
+      document.getElementById("search-input").style.display = "flex";
+    }, 250); //500 milisegundos
+  }
+}
